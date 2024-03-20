9fc4e7ba861a048e44b5bbb1474b5323a007b489
Priyan_ <119749952+priyan5295@users.noreply.github.com>
2024-03-20 22:55:33 +0530

    first commit-tailwind-portfolio

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1 className='text-3xl font-bold underline text-blue-400'>Hello its here </h1> */}
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Resume />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
